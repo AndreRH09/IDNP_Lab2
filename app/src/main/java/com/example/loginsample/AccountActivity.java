e3dec9f371d114f6f3f09b5956044b0bfc102d97
AndreRH09 <aanazcoh@unsa.edu.pe>
2024-09-25 18:39:53 -0500

    gestor result launcher agregado

--- app/src/main/java/com/example/loginsample/AccountActivity.java
+++ app/src/main/java/com/example/loginsample/AccountActivity.java
@@ -1,6 +1,9 @@
 package com.example.loginsample;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,31 @@ public class AccountActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btnAceptar = findViewById(R.id.btnAceptar);
+        Button btnCancelar= findViewById(R.id.btnCancelar);
+
+        EditText edtFirstname = findViewById(R.id.edtFirstname);
+        EditText edtLastname = findViewById(R.id.edtLastname);
+        EditText edtEmail = findViewById(R.id.edtEmail);
+        EditText edtPhone = findViewById(R.id.edtPhone);
+        EditText edtUsername2 = findViewById(R.id.edtUsername2);
+        EditText edtPassword2   = findViewById(R.id.edtPassword2);
+
+
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AccountEntity accountEntity = new AccountEntity();
+                accountEntity.setFirstname(edtFirstname.getText().toString());
+                accountEntity.setLastname(edtLastname.getText().toString());
+                accountEntity.setEmail(edtEmail.getText().toString());
+                accountEntity.setPhone(edtPhone.getText().toString());
+                accountEntity.setUsername(edtUsername2.getText().toString());
+                accountEntity.setPassword(edtPassword2.getText().toString());
+
+            }
+        });
     }
+
 }
