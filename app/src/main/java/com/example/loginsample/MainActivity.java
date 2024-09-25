0ad9b5374bd6c1644f86a05101431c83f3a0f75d
AndreRH09 <aanazcoh@unsa.edu.pe>
2024-09-25 11:31:43 -0500

    Funcionalidad de login username y passw

--- app/src/main/java/com/example/loginsample/MainActivity.java
+++ app/src/main/java/com/example/loginsample/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.loginsample;
 
+import android.app.Activity;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +15,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.loginsample.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static String TAG = "MainActivity";
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +31,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });*/
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        EditText edtUsername = binding.edtUsername;
+        EditText edtPassword = binding.edtPassword;
+
+        Button btnLogin = binding.btnLogin;
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(edtUsername.getText().toString().equals("admin") && edtPassword.getText().toString().equals("admin")){
+                    Toast.makeText(getApplicationContext(), "Bienvenido a mi App", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Bienvenido a mi App");
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Error en la Autentificacion", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Error en la Autentificacion");
+                }
+            }
+        });
     }
+
 }
